d6f95b31d597159962a839f9c9da08543b843daa
CerenKarayel <cerenkarayel95@gmail.com>
2024-08-15 00:41:38 +0300

    pages3

--- src/pages/OrderPizza.jsx
+++ src/pages/OrderPizza.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import "./OrderPizza.css";
 
 const OrderPizza = () => {
   return (
-    <div>
-      
-    </div>
+    <header className="header">
+            <h1>Teknolojik Yemekler</h1>
+            <nav className='link-container'>
+                <a href="#link1" className="anasayfa-link">Anasayfa</a>
+                <a href="#link2" className="siparis-link">Sipariş Oluştur</a>
+            </nav>
+        </header>
   )
 }
 
--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import "./Success.css";
 
 const Success = () => {
   return (
-    <div className=''>
+    <div className='onay-container'>
       <h1>Teknolojik Yemekler</h1>
+      <h2>TEBRİKLER!SİPARİŞİNİZ ALINDI!</h2>
     </div>
   )
 }
